4fad927795bcad6d5df799c920f5e57d6263508b
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-01-17 22:55:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            ArrayList<Integer> arr = new ArrayList<>();
+            
+            for(int i=0; i<n; i++)
+                arr.add(Integer.parseInt(S[i]));
+
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> res = ob.uniquePerms(arr,n);
+            for(int i=0; i<res.size(); i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    System.out.print(res.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+     // Helper function to generate unique permutations
+    public static void generate(ArrayList<Integer> num, int start, ArrayList<ArrayList<Integer> > result, HashSet<ArrayList<Integer>> h) {
+        // Base case: when start reaches the last index of the array
+        if (start == num.size() - 1) {
+            // Check if the permutation is already generated
+            if(h.contains(num))
+                return;
+            
+            // Create a new list with the current permutation
+            ArrayList<Integer> perm = new ArrayList<>(num);
+            
+            // Add the permutation to the set and result list
+            h.add(perm);
+            result.add(perm);
+            return;
+        } 
+        
+        Integer a, b;
+        
+        // Generate permutations by swapping elements starting from the given start index
+        for (int i = start; i < num.size(); i++) {
+            // Swap the elements at start and i positions
+            a = num.get(start);
+            b = num.get(i);
+            num.set(start, b);
+            num.set(i, a);
+            
+            // Recursive call to generate permutations for the next index
+            generate(num, start + 1, result, h);
+            
+            // Undo the swap to restore the original order for the next iteration
+            num.set(start, a);
+            num.set(i, b);
+        }
+    }
+    
+    
+    // Main function to generate unique permutations
+    public static ArrayList<ArrayList<Integer>> uniquePerms(ArrayList<Integer> arr ,int n) {
+        ArrayList<ArrayList<Integer> > result = new ArrayList<>();
+        
+        // If the input array is empty, return an empty list
+        if (arr.size() == 0)
+            return result;
+        
+        // Generate unique permutations and store them in the result list
+        generate(arr, 0, result, new HashSet<>());
+        
+        // Sort the result list based on element values and length
+        Collections.sort(result, (ArrayList<Integer> a, ArrayList<Integer> b) -> {
+            int i = 0, l = Math.min(a.size(), b.size());
+            
+            while(i < l) {
+                // Compare each element in the lists
+                if(a.get(i).compareTo(b.get(i)) != 0)
+                    return a.get(i).compareTo(b.get(i));
+                i++;
+            }
+            
+            // If the elements are equal, compare the lengths
+            return a.size() - b.size();
+        });
+        
+        // Return the sorted list of unique permutations
+        return result;
+    }
+    
+};
+
+
