5ad676763fdca4dd98ff43763cbaf00af4456efc
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-04-12 17:31:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Products/sum-of-products.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            long Arr[] = new long[N];
+            String[] S = read.readLine().split(" ");
+            for (int i = 0; i < N; i++) Arr[i] = Long.parseLong(S[i]);
+            Solution ob = new Solution();
+            System.out.println(ob.pairAndSum(N, Arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    static long pairAndSum(int n, long arr[]) {
+        // code here
+        long ans = 0;
+        
+        for(int i=0; i < 32; i++) {
+            long cnt = 0;
+            
+            for(int j=0; j < n; j++) {
+                if((arr[j] & (1L<<i)) != 0)  {
+                    cnt++;
+                }
+            }
+            
+            ans += (1<<i)*(cnt*(cnt-1)/2);
+        }
+        
+        return ans;
+    }
+    
+}
+
+
