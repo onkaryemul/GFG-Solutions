65956c2120eea98fd8fbbc4cf04d41304653dfe4
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-09-18 14:39:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement two stacks in an array/implement-two-stacks-in-an-array.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class TwoStack {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            twoStacks g = new twoStacks();
+            int Q = sc.nextInt();
+            while (Q > 0) {
+                int stack_no = sc.nextInt();
+                int QueryType = sc.nextInt();
+
+                if (QueryType == 1) {
+                    int a = sc.nextInt();
+                    if (stack_no == 1)
+                        g.push1(a);
+                    else if (stack_no == 2)
+                        g.push2(a);
+                } else if (QueryType == 2) {
+                    if (stack_no == 1)
+                        System.out.print(g.pop1() + " ");
+                    else if (stack_no == 2)
+                        System.out.print(g.pop2() + " ");
+                }
+
+                Q--;
+            }
+            System.out.println();
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class twoStacks {
+    
+    private int[] arr;
+    private int size;
+    private int top1;
+    private int top2;
+    
+    
+    twoStacks() {
+        size = 100; // given in the constraint
+        arr = new int[size];
+        top1 = -1;
+        top2 = size;
+    }
+
+
+    // Function to push an integer into the stack1.
+    void push1(int x) {
+        // Check if space exists
+        if(top2 - top1 > 1) {
+            arr[++top1] = x;
+        }
+    }
+
+
+    // Function to push an integer into the stack2.
+    void push2(int x) {
+        // Check if the space exists
+        if(top2 - top1 > 1) {
+            arr[--top2] = x;
+        }
+    }
+
+
+    // Function to remove an element from top of the stack1.
+    int pop1() {
+        if(top1 >= 0) {
+            int x = arr[top1--];
+            return x;
+        }
+        return -1;
+    }
+
+
+    // Function to remove an element from top of the stack2.
+    int pop2() {
+        if(top2 < size) {
+            int x = arr[top2++];
+            return x;
+        }
+        return -1;
+    }
+    
+}
+
