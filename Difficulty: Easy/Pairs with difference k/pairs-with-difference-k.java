39f56fa70080c3f101733a6680790581dbf9ea66
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-10-30 10:41:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with difference k/pairs-with-difference-k.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            int ans = ob.countPairsWithDiffK(arr, k);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    
+    // TC : O(n)
+    // SC : O(n)
+    
+    // Using HashMap
+    
+    // function to return the no. of pairs of integers whose difference equals a given no. k
+    int countPairsWithDiffK(int[] arr, int k) {
+        // code here
+        
+        // n1 - n2 = k
+        // hence, n2 = n1 - k
+        
+        // key : element of arr
+        // value : its frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int ele : arr) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        
+        int cnt = 0;
+        
+        for(int ele : arr) {
+            // here, ele => n1
+            int n2 = ele - k;
+            
+            if(map.containsKey(n2)) {
+                cnt += map.get(n2);
+            }
+        }
+        
+        return cnt;
+    }
+    
+}
+
+
