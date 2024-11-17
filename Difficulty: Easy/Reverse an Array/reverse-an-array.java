a134463bacbc206a653a734b8017b14ff03c2b74
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-11-17 11:27:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+
+            obj.reverseArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    
+    // TC : O(n)
+    // SC : O(1)
+    
+    // Reverse an Array in-place
+    
+    public void reverseArray(int arr[]) {
+        // code here
+        int n = arr.length;
+        
+        for(int i=0; i < n/2; i++) {
+            // swap arr[i] & arr[n-1-i]
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+        }
+    }
+    
+}
+
