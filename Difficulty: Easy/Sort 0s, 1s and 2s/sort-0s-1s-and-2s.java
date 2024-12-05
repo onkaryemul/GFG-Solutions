556b51908101b8a3334a9fa85b7a3c586c7a0e4c
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-12-05 08:30:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -11,11 +11,9 @@ class GFG {
         while (t-- > 0) {
             String input = br.readLine();
             String[] inputArray = input.split("\\s+");
-            ArrayList<Integer> a = new ArrayList<>();
+            int a[] = new int[inputArray.length];
 
-            for (String s : inputArray) {
-                a.add(Integer.parseInt(s));
-            }
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
 
             Solution ob = new Solution();
             ob.sort012(a);
@@ -24,61 +22,50 @@ class GFG {
                 System.out.print(num + " ");
             }
             System.out.println();
+            System.out.println("~");
         }
     }
 }
+
 
 // } Driver Code Ends
 
 
 class Solution {
     
-    // This problem is a variation of the popular "Dutch National flag algorithm". 
-    
-    /*
-        This algorithm contains 3 pointers i.e. low, mid, and high, and 3 main rules.  
-        The rules are the following:
-        
-        1. arr[0….low-1] contains 0. [Extreme left part]
-        2. arr[low….mid-1] contains 1.
-        3. arr[high+1….n-1] contains 2. [Extreme right part], n = size of the array
-        
-        The middle part i.e. arr[mid….high] is the unsorted segment. 
-    */
-
     // TC : O(n)
     // SC : O(1)
     
     // Function to sort an array of 0s, 1s, and 2s
-    public void sort012(ArrayList<Integer> arr) {
+    public void sort012(int[] arr) {
         // code here
-        int n = arr.size();
+        int n = arr.length;
         
-        int low = 0, mid = 0, high = n-1; // 3 pointers
+        int l = 0;
+        int r = n-1;
         
-        while(mid <= high) {
-            if(arr.get(mid) == 0) {
-                // Swapping arr[low] and arr[mid]
-                int temp = arr.get(low);
-                arr.set(low, arr.get(mid));
-                arr.set(mid, temp);
-                
-                low++;
-                mid++;
-            }
-            else if(arr.get(mid) == 1) {
-                mid++;
-            }
-            else if(arr.get(mid) == 2) {
-                // Swapping arr[mid] and arr[high]
-                int temp = arr.get(mid);
-                arr.set(mid, arr.get(high));
-                arr.set(high, temp);
-                
-                high--;
+        int i = 0;
+        
+        while(i <= r){
+            if(arr[i] == 0){
+                arr[i] = arr[l];
+                arr[l] = 0;
+                l++;
+                i++;
+            }else if(arr[i] == 2){
+                arr[i] = arr[r];
+                arr[r] = 2;
+                r--;
+            }else {
+                i++;
             }
         }
     }
     
 }
 
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
