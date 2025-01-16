7adc8b7f395d8d83df147b20e4b40f6f06bc42b1
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2025-01-16 11:04:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.java
+++ Difficulty: Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.java
@@ -1,75 +1,97 @@
 //{ Driver Code Starts
-import java.util.Scanner;
+import java.io.*;
 import java.util.*;
-import java.lang.*;
-import java.util.HashMap;
 
-class Largest_Subarray
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T > 0)
-        {
-            int N = sc.nextInt();
-            int a[] = new int[N];
-            for (int i = 0; i < N; i++) 
-                a[i] = sc.nextInt();
-            
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Create BufferedReader for efficient input reading
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            Solution g = new Solution();
-            int n = g.maxLen(a, a.length);
+        // Read number of test cases
+        int T = Integer.parseInt(br.readLine());
 
-            System.out.println(n);
+        // Process each test case
+        while (T-- > 0) {
+            // Read the entire line containing the array elements
+            String line = br.readLine();
 
-            T--;
+            // Split the line into an array of strings, then parse them as integers
+            String[] tokens = line.split("\\s+");
+            int[] a = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                a[i] = Integer.parseInt(tokens[i]);
+            }
+
+            // Create the Solution object
+            Solution obj = new Solution();
+
+            // Call maxLen function and print the result
+            System.out.println(obj.maxLen(a));
         }
     }
 }
-
 // } Driver Code Ends
-
-
 
 
 class Solution {
     
-    // TC : O(n)
-    // SC : O(n)
+    // TC : O(n) => 1 pass solution
+    // SC : O(n) => due to HashMap 
     
-    // return the maximum length of the subarray with equal 0s and 1s
-    int maxLen(int[] arr, int N)
-    {
+    // Using Prefix sum approach 
+    // Using the concept of "Longest subarray with sum K" => reverse engineering approach
+    
+    // Since, here arr[] contains only 0's and 1's
+    // So longest subarray with equal no. of 0's and 1's => will have sum as 0 (after modification of arr[] such that arr[i] = -1, for every arr[i]=0)
+    
+    // function to find the length of longest subarray with equal no. of 0's and 1's
+    public int maxLen(int[] arr) {
         // Your code here
+        int n = arr.length;
         
-        // arr[] : the input array containing 0s and 1s
-        // N : size of the input array
+        int maxLen = 0; // length of the longest subarray with equal no. of 0's and 1's
         
-        int maxLen = 0;
+        // For subarray with equal no. of 0's and 1's :
+        // we are adding -1 to the prefix sum wherever arr[i] = 0, and 1 for arr[i] = 1
+        // So that, that subarray with equal no. of 0's and 1's => will have sum = 0
         
-        // key: Sum, Value: Index
+        
+        // Therefore, this question is similar to "longest subarray with sum k"
+        // Here, we need to find "Longest subarray with sum 0", here k = 0
+        
+        int k = 0;
+        
         HashMap<Integer, Integer> map = new HashMap<>();
+        // key : prefix_sum
+        // value : index at which this prefix_sum occurred for the first time (leftmost) => in order to get longest subarrayy
+        
         map.put(0, -1); // 0 sum at index -1
         
-        int sum = 0;
+        int sum = 0; // running sum or prefix sum
         
-        for(int i=0; i < N; i++) {
+        for(int i=0; i < n; i++) {
             if(arr[i] == 0) {
-                sum += -1;
+                sum += (-1);
             }
-            else { // arr[i] == 1
+            else {
                 sum += 1;
             }
             
-            if(map.containsKey(sum)) {
-                int len = i - map.get(sum);
+            if(sum == k) {
+                maxLen = i + 1;
+            }
+            
+            // Check if remaining ie. (sum - k) previously appeared
+            if(map.containsKey(sum - k)) {
+                int len = i - map.get(sum - k);
                 maxLen = Math.max(maxLen, len);
             }
-            else {
+            
+            if(!map.containsKey(sum)) {
                 map.put(sum, i);
             }
         }
+        
         
         return maxLen;
     }
