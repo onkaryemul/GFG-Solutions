7538b68a8c92bb79698f93397576023f2b0a2301
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2025-07-30 20:04:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -1,76 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        // taking input using class Scanner
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            // taking total number of elements
-            int k = Integer.parseInt(br.readLine());
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-            int res = new Solution().countSubarrays(arr, k);
-
-            System.out.print(res);
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
     
     // TC : O(n)
-    // SC : O(n), due to hashmap
-    
-    // Optimal approach => O(n) => single pass solution
-    
-    // Using Prefix sum approach
-    
-    // Using Reverse Engineering technique
-    // No. of subarrays ending at index i with sum as k 
-    // = prefix sum till index i (prefixSum) - no. of subarrays with sum =(prefixSum - k) in that range
-    
-    public int countSubarrays(int arr[], int k) {
+    // SC : O(n)
+
+    // function to count the number of subarrays whose sum exactly equal to given number k    
+    public int cntSubarrays(int[] arr, int k) {
         // code here
         int n = arr.length;
         
-        // key : prefix sum till that index i
-        // value : count of subarrays with that prefix sum
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int cnt = 0;
         
-        int prefixSum = 0;
-        int cnt = 0; // count of subarrays with sum exactly equal to k
-        
-        // prefixSum = 0, with count = 1
+        // key: prefix sum till curr index
+        // value: count of this prefix sum
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         map.put(0, 1);
         
+        int prefixSum = 0;
+        
         for(int i=0; i < n; i++) {
-            prefixSum += arr[i];
+            prefixSum += arr[i]; // prefix sum till curr index i
             
-            int remove = prefixSum - k;
+            int remove = prefixSum - k; 
+            
+            // appyling reverse engineering principle
+            // need to find how many subarrays whose sum (prefixSum - k) have occurred previously to get no. of subarrays ending at curr index i and having sum is equal to k
             
             if(map.containsKey(remove)) {
                 cnt += map.get(remove);
@@ -83,4 +36,3 @@ class Solution {
     }
     
 }
-
